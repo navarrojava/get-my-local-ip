811ade3583b8479833591df965d2d26aa82c7242
Navarro Ferreira <navarro.pod@gmail.com>
2017-04-12 22:14:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const os = require("os");
+
+const netWorkAdaptersInfo = os.networkInterfaces();
+
+let localAdapter = Object.keys(netWorkAdaptersInfo).map((a) =>
+    netWorkAdaptersInfo[a].reduce((prev, curr) =>
+        (curr['address'].includes('192')) ||
+        (curr['address'].includes('172')) ||
+        (curr['address'].includes('10'))
+            ? curr : prev, null)
+).filter(Boolean);
+
+let localInternetConnection = Object.assign({}, localAdapter)[0];
+
+module.exports = localInternetConnection;
