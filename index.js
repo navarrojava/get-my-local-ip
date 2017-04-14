b2101b55398fadad048eb0f1f33852f0d538918b
Navarro Ferreira <navarro.pod@gmail.com>
2017-04-14 16:18:39 -0300

    fix issue when whithout network connection, returns 127.0.0.1 as network adress

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ let localAdapter = Object.keys(netWorkAdaptersInfo).map((a) =>
     netWorkAdaptersInfo[a].reduce((prev, curr) =>
         (curr['address'].includes('192')) ||
         (curr['address'].includes('172')) ||
-        (curr['address'].includes('10'))
+        (curr['address'].includes('10'))  ||
+        (curr['address'].includes('127'))
             ? curr : prev, null)
 ).filter(Boolean);
 
