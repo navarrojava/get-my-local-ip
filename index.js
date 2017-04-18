7d0312553696ac1973f2462f2aad673d371426fb
Navarro Ferreira <navarro.pod@gmail.com>
2017-04-18 19:07:15 -0300

    adjust

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const os = require("os");
 
+
+const http = require("http");
+
 const netWorkAdaptersInfo = os.networkInterfaces();
+
+let local = { address: '127.0.0.1',
+    netmask: '255.0.0.0',
+    family: 'IPv4',
+    mac: '00:00:00:00:00:00',
+    internal: true };
+
 
 let localAdapter = Object.keys(netWorkAdaptersInfo).map((a) =>
     netWorkAdaptersInfo[a].reduce((prev, curr) =>
         (curr['address'].includes('192')) ||
         (curr['address'].includes('172')) ||
-        (curr['address'].includes('10'))  ||
-        (curr['address'].includes('127'))
+        (curr['address'].includes('10'))
             ? curr : prev, null)
 ).filter(Boolean);
 
 let localInternetConnection = Object.assign({}, localAdapter)[0];
 
+
+
 module.exports = localInternetConnection;
+
+
+
